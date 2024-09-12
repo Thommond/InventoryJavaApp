8020b2cff966f5c331ed6b48d81cf01248230a18
Thom Mondeaux <Thommond554@gmail.com>
2024-09-12 16:00:07 -0400

    Added the Product class's getters and setters.

--- Product.java
+++ Product.java
@@ -8,15 +8,13 @@ public class Product {
 
        // Default constructor
        public Product() {
-        // Initializes with default values
         this.productID = 0;
         this.productName = "";
         this.category = 0;
         this.price = 0.0;
         this.quantityInStock = 0;
     }
-
-    // Full constructor
+    
     public Product(int productID, String productName, int category, double price, int quantityInStock) {
         this.productID = productID;
         this.productName = productName;
@@ -25,5 +23,48 @@ public class Product {
         this.quantityInStock = quantityInStock;
     }
 
-    
+      // Getter and Setter for productID
+      public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    // Getter and Setter for productName
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    // Getter and Setter for category
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    // Getter and Setter for price
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Getter and Setter for quantityInStock
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
 }
