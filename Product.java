d692d16f530e09032e87509d823cc28af6526569
Thom Mondeaux <Thommond554@gmail.com>
2024-09-12 15:54:20 -0400

    Started the product class with its variables and contructors.

--- Product.java
+++ Product.java
@@ -1,3 +1,29 @@
 public class Product {
+
+    private int productID;
+    private String productName;
+    private int category;
+    private double price; 
+    private int quantityInStock;
+
+       // Default constructor
+       public Product() {
+        // Initializes with default values
+        this.productID = 0;
+        this.productName = "";
+        this.category = 0;
+        this.price = 0.0;
+        this.quantityInStock = 0;
+    }
+
+    // Full constructor
+    public Product(int productID, String productName, int category, double price, int quantityInStock) {
+        this.productID = productID;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+        this.quantityInStock = quantityInStock;
+    }
+
     
 }
